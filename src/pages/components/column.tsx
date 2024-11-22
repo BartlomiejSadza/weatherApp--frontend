9d4053c4366c3af169e116d77d6c618514274159
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-11-22 20:07:35 +0100

    pora na wyszukiwanko po mieście, bo tak wymyśliłem.

--- src/pages/components/column.tsx
+++ src/pages/components/column.tsx
@@ -1,5 +1,5 @@
 import styles from '@/styles/column.module.css';
-
+import ikonki from '@/utils/ikonki'
 
 interface ColumnProps {
     date: string;
@@ -19,15 +19,17 @@ function formatDaty(dataString: string): string {
 export default function Column(columnProps: ColumnProps): JSX.Element {
     const date = (columnProps.date);
     const weatherCode = columnProps.weatherCode;
-    const maxTemp = columnProps.temperature2mMax;
-    const minTemp = columnProps.temperature2mMin;
-    const estimatedEnergy = columnProps.estimatedEnergy;  
+    const maxTemp = columnProps.temperature2mMax.toFixed(1);
+    const minTemp = columnProps.temperature2mMin.toFixed(1);
+    const estimatedEnergy = columnProps.estimatedEnergy.toFixed(2);  
+
+    const ikonka = ikonki[weatherCode];
 
   return <div className={styles.column}>
     <h2>{formatDaty(date)}</h2>
-    <p>Weather Code: {weatherCode}</p>
-    <p>Max Temp: {}</p>
-    <p>Min Temp: {}</p>
-    <p>Estimated Energy: {}</p>
+    <img src={ikonka} alt={`ikonka dla weather code: ${weatherCode}`}/>
+    <p>Max Temp: {maxTemp}</p>
+    <p>Min Temp: {minTemp}</p>
+    <p>Estimated Energy: {estimatedEnergy} kWh</p>
   </div>;
 }
---
+++ src/utils/ikonki.ts
@@ -0,0 +1,32 @@
+const iconMap: { [key: number]: string } = {
+    0: 'https://openweathermap.org/img/wn/01d.png', // Clear sky
+    1: 'https://openweathermap.org/img/wn/02d.png', // Mainly clear
+    2: 'https://openweathermap.org/img/wn/03d.png', // Partly cloudy
+    3: 'https://openweathermap.org/img/wn/04d.png', // Overcast
+    45: 'https://openweathermap.org/img/wn/50d.png', // Fog
+    48: 'https://openweathermap.org/img/wn/50d.png', // Depositing rime fog
+    51: 'https://openweathermap.org/img/wn/09d.png', // Drizzle light
+    53: 'https://openweathermap.org/img/wn/09d.png', // Drizzle moderate
+    55: 'https://openweathermap.org/img/wn/09d.png', // Drizzle dense
+    56: 'https://openweathermap.org/img/wn/09d.png', // Freezing drizzle light
+    57: 'https://openweathermap.org/img/wn/09d.png', // Freezing drizzle dense
+    61: 'https://openweathermap.org/img/wn/10d.png', // Rain slight
+    63: 'https://openweathermap.org/img/wn/10d.png', // Rain moderate
+    65: 'https://openweathermap.org/img/wn/10d.png', // Rain heavy
+    66: 'https://openweathermap.org/img/wn/13d.png', // Freezing rain light
+    67: 'https://openweathermap.org/img/wn/13d.png', // Freezing rain heavy
+    71: 'https://openweathermap.org/img/wn/13d.png', // Snow fall slight
+    73: 'https://openweathermap.org/img/wn/13d.png', // Snow fall moderate
+    75: 'https://openweathermap.org/img/wn/13d.png', // Snow fall heavy
+    77: 'https://openweathermap.org/img/wn/13d.png', // Snow grains
+    80: 'https://openweathermap.org/img/wn/09d.png', // Rain showers slight
+    81: 'https://openweathermap.org/img/wn/09d.png', // Rain showers moderate
+    82: 'https://openweathermap.org/img/wn/09d.png', // Rain showers violent
+    85: 'https://openweathermap.org/img/wn/13d.png', // Snow showers slight
+    86: 'https://openweathermap.org/img/wn/13d.png', // Snow showers heavy
+    95: 'https://openweathermap.org/img/wn/11d.png', // Thunderstorm slight
+    96: 'https://openweathermap.org/img/wn/11d.png', // Thunderstorm hail slight
+    99: 'https://openweathermap.org/img/wn/11d.png', // Thunderstorm hail heavy
+  };
+  
+export default iconMap;
